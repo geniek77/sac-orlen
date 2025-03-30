f1c8036aecc9be987e77f07e0eb07c47136530ef
geniek77 <eugeniusz@szmytkowski.pl>
2025-03-30 10:53:52 +0200

    ok
    ok

--- kalendarz_miesieczny_2/kalendarz_miesieczny_2_builder.js
+++ kalendarz_miesieczny_2/kalendarz_miesieczny_2_builder.js
@@ -1,119 +1,88 @@
 (function () {
-  let template = document.createElement("template");
-  template.innerHTML = `
-    <form id="form">
-      <fieldset>
-        <legend>Ustawienia kalendarza</legend>
-
-        <label>Minimalny rok:</label><br>
-        <input type="number" id="minYear"><br><br>
-
-        <label>Maksymalny rok:</label><br>
-        <input type="number" id="maxYear"><br><br>
-
-        <label>Miesiące numerycznie:</label><br>
-        <input type="checkbox" id="numericMonths"><br><br>
-
-        <label>Kolor tła miesięcy:</label><br>
-        <input type="color" id="monthBgColor"><br><br>
-
-        <label>Kolor tła kwartałów:</label><br>
-        <input type="color" id="quarterBgColor"><br><br>
-
-        <label>Kolor czcionki:</label><br>
-        <input type="color" id="fontColor"><br><br>
-
-        <label>Czcionka (np. Arial):</label><br>
-        <input type="text" id="fontFamily"><br><br>
-
-        <label>Kolor aktywnej komórki:</label><br>
-        <input type="color" id="activeCellColor"><br><br>
-
-        <label>Kolor tła przycisków:</label><br>
-        <input type="color" id="buttonColor"><br><br>
-
-        <label>Kolor tekstu przycisków:</label><br>
-        <input type="color" id="buttonTextColor"><br><br>
-
-        <button type="submit">Zapisz</button>
-      </fieldset>
-    </form>
-  `;
-
   class KalendarzMiesieczny2Builder extends HTMLElement {
     constructor() {
       super();
-      this._shadowRoot = this.attachShadow({ mode: "open" });
-      this._shadowRoot.appendChild(template.content.cloneNode(true));
 
-      this._shadowRoot.getElementById("form").addEventListener("submit", e => {
+      this.innerHTML = `
+        <form id="form">
+          <fieldset>
+            <legend>Ustawienia kalendarza</legend>
+
+            <label>Minimalny rok:</label><br>
+            <input type="number" id="minYear"><br><br>
+
+            <label>Maksymalny rok:</label><br>
+            <input type="number" id="maxYear"><br><br>
+
+            <label>Miesiące numerycznie:</label><br>
+            <input type="checkbox" id="numericMonths"><br><br>
+
+            <label>Kolor tła miesięcy:</label><br>
+            <input type="color" id="monthBgColor"><br><br>
+
+            <label>Kolor tła kwartałów:</label><br>
+            <input type="color" id="quarterBgColor"><br><br>
+
+            <label>Kolor czcionki:</label><br>
+            <input type="color" id="fontColor"><br><br>
+
+            <label>Czcionka (np. Arial):</label><br>
+            <input type="text" id="fontFamily"><br><br>
+
+            <label>Kolor aktywnej komórki:</label><br>
+            <input type="color" id="activeCellColor"><br><br>
+
+            <label>Kolor tła przycisków:</label><br>
+            <input type="color" id="buttonColor"><br><br>
+
+            <label>Kolor tekstu przycisków:</label><br>
+            <input type="color" id="buttonTextColor"><br><br>
+
+            <button type="submit">Zapisz</button>
+          </fieldset>
+        </form>
+      `;
+
+      this.querySelector("#form").addEventListener("submit", (e) => {
         e.preventDefault();
         this._updateProperties();
       });
     }
 
-    // GET
     getProperties() {
       return {
-        minYear: this.minYear,
-        maxYear: this.maxYear,
-        numericMonths: this.numericMonths,
-        monthBgColor: this.monthBgColor,
-        quarterBgColor: this.quarterBgColor,
-        fontColor: this.fontColor,
-        fontFamily: this.fontFamily,
-        activeCellColor: this.activeCellColor,
-        buttonColor: this.buttonColor,
-        buttonTextColor: this.buttonTextColor
+        minYear: parseInt(this.querySelector("#minYear").value),
+        maxYear: parseInt(this.querySelector("#maxYear").value),
+        numericMonths: this.querySelector("#numericMonths").checked,
+        monthBgColor: this.querySelector("#monthBgColor").value,
+        quarterBgColor: this.querySelector("#quarterBgColor").value,
+        fontColor: this.querySelector("#fontColor").value,
+        fontFamily: this.querySelector("#fontFamily").value,
+        activeCellColor: this.querySelector("#activeCellColor").value,
+        buttonColor: this.querySelector("#buttonColor").value,
+        buttonTextColor: this.querySelector("#buttonTextColor").value
       };
     }
 
-    // SET
     setProperties(props) {
-      this.minYear = props.minYear ?? 2000;
-      this.maxYear = props.maxYear ?? 2035;
-      this.numericMonths = props.numericMonths ?? false;
-      this.monthBgColor = props.monthBgColor ?? "#ffffff";
-      this.quarterBgColor = props.quarterBgColor ?? "#eeeeee";
-      this.fontColor = props.fontColor ?? "#000000";
-      this.fontFamily = props.fontFamily ?? "Arial";
-      this.activeCellColor = props.activeCellColor ?? "lightblue";
-      this.buttonColor = props.buttonColor ?? "#f0f0f0";
-      this.buttonTextColor = props.buttonTextColor ?? "#000000";
-
-      this._updateFormUI();
+      this.querySelector("#minYear").value = props.minYear ?? 2000;
+      this.querySelector("#maxYear").value = props.maxYear ?? 2035;
+      this.querySelector("#numericMonths").checked = !!props.numericMonths;
+      this.querySelector("#monthBgColor").value = props.monthBgColor ?? "#ffffff";
+      this.querySelector("#quarterBgColor").value = props.quarterBgColor ?? "#eeeeee";
+      this.querySelector("#fontColor").value = props.fontColor ?? "#000000";
+      this.querySelector("#fontFamily").value = props.fontFamily ?? "Arial";
+      this.querySelector("#activeCellColor").value = props.activeCellColor ?? "#3399ff";
+      this.querySelector("#buttonColor").value = props.buttonColor ?? "#f0f0f0";
+      this.querySelector("#buttonTextColor").value = props.buttonTextColor ?? "#000000";
     }
 
-    // Przypisz dane do formularza
-    _updateFormUI() {
-      this._shadowRoot.getElementById("minYear").value = this.minYear;
-      this._shadowRoot.getElementById("maxYear").value = this.maxYear;
-      this._shadowRoot.getElementById("numericMonths").checked = this.numericMonths;
-      this._shadowRoot.getElementById("monthBgColor").value = this.monthBgColor;
-      this._shadowRoot.getElementById("quarterBgColor").value = this.quarterBgColor;
-      this._shadowRoot.getElementById("fontColor").value = this.fontColor;
-      this._shadowRoot.getElementById("fontFamily").value = this.fontFamily;
-      this._shadowRoot.getElementById("activeCellColor").value = this.activeCellColor;
-      this._shadowRoot.getElementById("buttonColor").value = this.buttonColor;
-      this._shadowRoot.getElementById("buttonTextColor").value = this.buttonTextColor;
-    }
-
-    // Odczyt z formularza do obiektu klasy + wysyłka do SAC
     _updateProperties() {
-      this.minYear = parseInt(this._shadowRoot.getElementById("minYear").value);
-      this.maxYear = parseInt(this._shadowRoot.getElementById("maxYear").value);
-      this.numericMonths = this._shadowRoot.getElementById("numericMonths").checked;
-      this.monthBgColor = this._shadowRoot.getElementById("monthBgColor").value;
-      this.quarterBgColor = this._shadowRoot.getElementById("quarterBgColor").value;
-      this.fontColor = this._shadowRoot.getElementById("fontColor").value;
-      this.fontFamily = this._shadowRoot.getElementById("fontFamily").value;
-      this.activeCellColor = this._shadowRoot.getElementById("activeCellColor").value;
-      this.buttonColor = this._shadowRoot.getElementById("buttonColor").value;
-      this.buttonTextColor = this._shadowRoot.getElementById("buttonTextColor").value;
-
+      const props = this.getProperties();
+      console.log("Wysłano propertiesChanged:", props);
       this.dispatchEvent(new CustomEvent("propertiesChanged", {
         detail: {
-          properties: this.getProperties()
+          properties: props
         }
       }));
     }
