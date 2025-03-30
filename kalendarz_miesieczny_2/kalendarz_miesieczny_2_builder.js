c226fa25218205cfd2c1bf352a361e94e243d15a
geniek77 <eugeniusz@szmytkowski.pl>
2025-03-30 12:11:03 +0200

    Update kalendarz_miesieczny_2_builder.js

--- kalendarz_miesieczny_2/kalendarz_miesieczny_2_builder.js
+++ kalendarz_miesieczny_2/kalendarz_miesieczny_2_builder.js
@@ -46,62 +46,106 @@
       this._shadowRoot = this.attachShadow({ mode: "open" });
       this._shadowRoot.appendChild(template.content.cloneNode(true));
 
-      this._shadowRoot.getElementById("form").addEventListener("submit", (e) => {
+      this._shadowRoot.getElementById("form").addEventListener("submit", e => {
         e.preventDefault();
-        this._updateProperties();
+        this._firePropertiesChanged();
       });
     }
 
+    // Wysyłanie eventu do SAC
+    _firePropertiesChanged() {
+      this.dispatchEvent(new CustomEvent("propertiesChanged", {
+        detail: {
+          properties: this.getProperties()
+        }
+      }));
+    }
+
+    // Pobieranie wszystkich właściwości
     getProperties() {
       return {
-        minYear: parseInt(this._shadowRoot.getElementById("minYear").value),
-        maxYear: parseInt(this._shadowRoot.getElementById("maxYear").value),
-        numericMonths: this._shadowRoot.getElementById("numericMonths").checked,
-        monthBgColor: this._shadowRoot.getElementById("monthBgColor").value,
-        quarterBgColor: this._shadowRoot.getElementById("quarterBgColor").value,
-        fontColor: this._shadowRoot.getElementById("fontColor").value,
-        fontFamily: this._shadowRoot.getElementById("fontFamily").value,
-        activeCellColor: this._shadowRoot.getElementById("activeCellColor").value,
-        buttonColor: this._shadowRoot.getElementById("buttonColor").value,
-        buttonTextColor: this._shadowRoot.getElementById("buttonTextColor").value
+        minYear: this.minYear,
+        maxYear: this.maxYear,
+        numericMonths: this.numericMonths,
+        monthBgColor: this.monthBgColor,
+        quarterBgColor: this.quarterBgColor,
+        fontColor: this.fontColor,
+        fontFamily: this.fontFamily,
+        activeCellColor: this.activeCellColor,
+        buttonColor: this.buttonColor,
+        buttonTextColor: this.buttonTextColor
       };
     }
 
-    setProperties(properties) {
-      const safe = (val, def) => (val !== undefined && val !== null ? val : def);
-
-      const props = {
-        minYear: parseInt(safe(properties.minYear, 2000)),
-        maxYear: parseInt(safe(properties.maxYear, 2035)),
-        numericMonths: ("" + properties.numericMonths) === "true",
-        monthBgColor: safe(properties.monthBgColor, "#ffffff"),
-        quarterBgColor: safe(properties.quarterBgColor, "#eeeeee"),
-        fontColor: safe(properties.fontColor, "#000000"),
-        fontFamily: safe(properties.fontFamily, "Arial"),
-        activeCellColor: safe(properties.activeCellColor, "lightblue"),
-        buttonColor: safe(properties.buttonColor, "#f0f0f0"),
-        buttonTextColor: safe(properties.buttonTextColor, "#000000")
-      };
-
-      requestAnimationFrame(() => {
-        this._shadowRoot.getElementById("minYear").value = props.minYear;
-        this._shadowRoot.getElementById("maxYear").value = props.maxYear;
-        this._shadowRoot.getElementById("numericMonths").checked = props.numericMonths;
-        this._shadowRoot.getElementById("monthBgColor").value = props.monthBgColor;
-        this._shadowRoot.getElementById("quarterBgColor").value = props.quarterBgColor;
-        this._shadowRoot.getElementById("fontColor").value = props.fontColor;
-        this._shadowRoot.getElementById("fontFamily").value = props.fontFamily;
-        this._shadowRoot.getElementById("activeCellColor").value = props.activeCellColor;
-        this._shadowRoot.getElementById("buttonColor").value = props.buttonColor;
-        this._shadowRoot.getElementById("buttonTextColor").value = props.buttonTextColor;
-      });
+    // Indywidualne gettery i settery
+    get minYear() {
+      return parseInt(this._shadowRoot.getElementById("minYear").value);
+    }
+    set minYear(value) {
+      this._shadowRoot.getElementById("minYear").value = value;
     }
 
-    _updateProperties() {
-      const props = this.getProperties();
-      this.dispatchEvent(new CustomEvent("propertiesChanged", {
-        detail: { properties: props }
-      }));
+    get maxYear() {
+      return parseInt(this._shadowRoot.getElementById("maxYear").value);
+    }
+    set maxYear(value) {
+      this._shadowRoot.getElementById("maxYear").value = value;
+    }
+
+    get numericMonths() {
+      return this._shadowRoot.getElementById("numericMonths").checked;
+    }
+    set numericMonths(value) {
+      this._shadowRoot.getElementById("numericMonths").checked = value;
+    }
+
+    get monthBgColor() {
+      return this._shadowRoot.getElementById("monthBgColor").value;
+    }
+    set monthBgColor(value) {
+      this._shadowRoot.getElementById("monthBgColor").value = value;
+    }
+
+    get quarterBgColor() {
+      return this._shadowRoot.getElementById("quarterBgColor").value;
+    }
+    set quarterBgColor(value) {
+      this._shadowRoot.getElementById("quarterBgColor").value = value;
+    }
+
+    get fontColor() {
+      return this._shadowRoot.getElementById("fontColor").value;
+    }
+    set fontColor(value) {
+      this._shadowRoot.getElementById("fontColor").value = value;
+    }
+
+    get fontFamily() {
+      return this._shadowRoot.getElementById("fontFamily").value;
+    }
+    set fontFamily(value) {
+      this._shadowRoot.getElementById("fontFamily").value = value;
+    }
+
+    get activeCellColor() {
+      return this._shadowRoot.getElementById("activeCellColor").value;
+    }
+    set activeCellColor(value) {
+      this._shadowRoot.getElementById("activeCellColor").value = value;
+    }
+
+    get buttonColor() {
+      return this._shadowRoot.getElementById("buttonColor").value;
+    }
+    set buttonColor(value) {
+      this._shadowRoot.getElementById("buttonColor").value = value;
+    }
+
+    get buttonTextColor() {
+      return this._shadowRoot.getElementById("buttonTextColor").value;
+    }
+    set buttonTextColor(value) {
+      this._shadowRoot.getElementById("buttonTextColor").value = value;
     }
   }
 
