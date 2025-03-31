eecf79d267a2dfb739cc929c5845ecfca4ed0560
geniek77 <eugeniusz@szmytkowski.pl>
2025-03-31 09:49:57 +0200

    ok

--- orlenKPI/orlenKPI_builder.js
+++ orlenKPI/orlenKPI_builder.js
@@ -83,7 +83,7 @@
       super();
       this._shadowRoot = this.attachShadow({ mode: "open" });
       this._shadowRoot.appendChild(template.content.cloneNode(true));
-      const form = this._shadowRoot.querySelector("form");
+      const form = this._shadowRoot.getElementById("form");
       if (form) {
         form.addEventListener("submit", e => {
         e.preventDefault();
